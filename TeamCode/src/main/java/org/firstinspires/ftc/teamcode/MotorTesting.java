d1cd422ae3e5067fc1512dd885500a4b8302707a
Hamish Milne <darkhamdo21@gmail.com>
2025-12-08 09:56:41 +1300

    rising edge dectector. Adjusted spindexer positions and timing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTesting.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 @TeleOp
@@ -13,6 +14,17 @@ public class MotorTesting extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        final double Spindexder_RPM = 117;
+        final double Seconds_Per_Rev = (1/(Spindexder_RPM /60));
+        final double Spindexer_move_120 = Seconds_Per_Rev/3;
+        final double Servo_Start_Position = 0.05;
+        final double IntakeServoEndPosition = 0.2;
+        final double OuttakeServoEndPosition = 0.3;
+
+        final double SpindexerEncoderPulsesPerRevolution = (1 + (46.0 / 17.0)) * (1 + (46.0 / 17.0)) * (1 + (46.0 / 17.0)) * 28;
+
+
         // Use DcMotorEx for advanced features like current sensing
         DcMotorEx frontLeftMotor = hardwareMap.get(DcMotorEx.class, "FrontLeftMotor");
         DcMotorEx backLeftMotor = hardwareMap.get(DcMotorEx.class, "BackLeftMotor");
@@ -22,18 +34,33 @@ public class MotorTesting extends LinearOpMode {
         DcMotorEx RightOuttakeMotor = hardwareMap.get(DcMotorEx.class, "RightOuttakeMotor");
         DcMotorEx IntakeMotor = hardwareMap.get(DcMotorEx.class, "IntakeMotor");
         DcMotorEx SpindexerMotor = hardwareMap.get(DcMotorEx.class, "SpindexerMotor");
+        Servo IntakeServo = hardwareMap.get(Servo.class, "IntakeServo");
+        Servo OuttakeServo = hardwareMap.get(Servo.class, "OuttakeServo");
+
+
 
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         SpindexerMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        LeftOuttakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RightOuttakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         SpindexerMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        double spinDexerRotation = 0;
+
+
+
+        boolean previousGamepadY = false;
 
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
+
+            IntakeServo.setPosition(Servo_Start_Position);
+            OuttakeServo.setPosition(Servo_Start_Position);
+
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x * 1.1;
             double rx = gamepad1.right_stick_x;
@@ -64,9 +91,27 @@ public class MotorTesting extends LinearOpMode {
                 sleep(1500);
                 IntakeMotor.setPower(0);
             }
+            if (gamepad1.y && !previousGamepadY){
 
+                // rotates by 1/3 of a total revolution
+                spinDexerRotation += SpindexerEncoderPulsesPerRevolution/3;
+
+                SpindexerMotor.setTargetPosition((int) spinDexerRotation);
                 SpindexerMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                SpindexerMotor.setPower(0.3);
+            }
+            if (gamepad1.dpad_up){
+                IntakeServo.setPosition(IntakeServoEndPosition);
+                sleep(1000);
+                IntakeServo.setPosition(Servo_Start_Position);
+            }
+            if (gamepad1.dpad_down){
+                OuttakeServo.setPosition(OuttakeServoEndPosition);
+                sleep(1000);
+                OuttakeServo.setPosition(Servo_Start_Position);
+            }
 
+            previousGamepadY = gamepad1.y;
 
 
 
@@ -74,6 +119,9 @@ public class MotorTesting extends LinearOpMode {
             telemetry.addData("Left Outtake Motor Current (mA)", LeftOuttakeMotor.getCurrent(CurrentUnit.MILLIAMPS));
             telemetry.addData("Right Outtake Motor Current (mA)", RightOuttakeMotor.getCurrent(CurrentUnit.MILLIAMPS));
             telemetry.addData("Intake Motor Current (mA)",IntakeMotor.getCurrent(CurrentUnit.MILLIAMPS));
+            telemetry.addData("Intake Servo Position",IntakeServo.getPosition());
+            telemetry.addData("Outtake Servo Position",OuttakeServo.getPosition());
+            telemetry.addData("Spindexer Encoder Counts", spinDexerRotation);
 
             telemetry.update();
         }
